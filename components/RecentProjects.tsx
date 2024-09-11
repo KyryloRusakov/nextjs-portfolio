abd8d648a5a479535463402ea785b72f8b773c24
Kyrylo Rusakov <Kyrylo_Rusakov@epam.com>
2024-09-11 21:00:00 +0300

    change pathes

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -16,7 +16,7 @@ const RecentProjects = () => {
             <PinContainer title={link} href={link}>
               <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                  <img src="/bg.png" alt="bg image" />
+                  <img src="/nextjs-portfolio/bg.png" alt="bg image" />
                 </div>
                 <img src={img} alt={title} className="z-10 absolute bottom-0"/>
               </div>
